22c457d867582904ff8030b3cddb4d8bcba23c96
Tun <116370663+tun784@users.noreply.github.com>
2025-02-24 10:44:21 +0700

    24-02-2025 10:44

--- Chuong1/Mang1Chieu.java
+++ Chuong1/Mang1Chieu.java
@@ -15,7 +15,10 @@ public class Mang1Chieu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        int[] arr = new int[30];
+        int n = 0;
+        System.out.print("Nhap n= ");
+        n = sc.nextInt();
+        int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(399)-199;
         }
